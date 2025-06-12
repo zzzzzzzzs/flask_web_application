dea6fc61c39fbb8e7a6220d53bbba3854749e938
zouzhisheng <tristanzou@tungee.com>
2025-06-12 18:45:21 +0800

    modify unit test

--- tests/unit/test_api_user.py
+++ tests/unit/test_api_user.py
@@ -3,7 +3,7 @@ from tests.conftest import client
 
 def test_user_login(client):
     rsp = client.post('/api/user/login/')
-    assert rsp.status_code == 200
+    assert rsp.status_code == 400
 
 def test_get_users(client):
     pass
